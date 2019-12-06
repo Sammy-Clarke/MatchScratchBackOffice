24d9ea7c27f33c142b0574f63ccd820c8ebf9766
Sammy-Clarke <samantha.clarke@footballpools.com>
2019-12-06 16:43:05 +0000

    Add project files.

---
+++ MatchScratchBackOffice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatchScratchBackOffice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatchScratchBackOffice")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b67aded2-f7da-459e-97bb-b32ffff4239e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MatchScratchBackOffice/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AutoFrame.Core.Base;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using AutoFrame.Services.Services.StopWatchService;
+using MatchScratchPageComponent.Pages;
+
+namespace MatchScratchBackOffice
+{
+    [TestClass]
+    public class UnitTest1 : BaseTest
+    {
+        [TestMethod]
+        public void CreateRound()
+        {
+            Logger.Info("\nEnter TestTimer() method. ");
+            IStopWatchService stopWatchService = Container.Resolve<IStopWatchService>();
+            stopWatchService.StartTimer();
+            MatchScratchLoginPage matchScatchLoginPage = NavigateTo<MatchScratchLoginPage>(ServerURL);
+            var duration = stopWatchService.StopTimer();
+            Console.WriteLine("Duration=" + duration);
+            Logger.Info("\nExit TestTimer() method. ");
+        }
+    }
+    
+}
